63b4231140539a6791c82f6aa3d230d2da833d2c
Blackhunter <58085293+omarprocessor@users.noreply.github.com>
2025-03-17 15:47:12 +0300

    Update smartirrigation.ino

--- smartirrigation.ino
+++ smartirrigation.ino
@@ -1,49 +1,75 @@
 #include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
+#include <DHT.h>
 
+// Wi-Fi Access Point credentials
 const char *ssid = "ESP8266_AP";
 const char *password = "12345678";
 
+// Create a web server on port 80
 WiFiServer server(80);
-SoftwareSerial sim800l(D5, D6); // RX, TX
+
+// Define software serial for SIM800L module (D5 = RX, D6 = TX)
+SoftwareSerial sim800l(D5, D6);
+
+// Sensor Pins
+#define DHTPIN D4  // DHT11 data pin
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+#define LDRPIN A0     // LDR connected to analog pin A0
+#define SOILPIN D3    // Soil moisture sensor connected to D3
 
 void setup()
 {
+    // Start the ESP8266 as an Access Point
     WiFi.softAP(ssid, password);
-    server.begin();
+    server.begin();  // Start the web server
+
+    // Initialize SIM800L communication
     sim800l.begin(9600);
+
+    // Initialize sensors
+    dht.begin();
+    pinMode(SOILPIN, INPUT);
 }
 
 void loop()
 {
+    // Read sensor values
+    int brightness = analogRead(LDRPIN);        // LDR sensor (0-1023)
+    float temperature = dht.readTemperature();  // DHT11 temperature (°C)
+    float humidity = dht.readHumidity();        // DHT11 humidity (%)
+    int soilMoisture = digitalRead(SOILPIN);    // Soil moisture (0 or 1)
+
+    // Check for incoming web clients
     WiFiClient client = server.available();
     if (client)
     {
-        String request = client.readStringUntil('\r');
+        String request = client.readStringUntil('\r'); // Read HTTP request
         client.flush();
 
+        // Handle request for the average sensor data API endpoint
         if (request.indexOf("/average") != -1)
         {
-            float avgBrightness = 512;
-            float avgTemperature = 25;
-            float avgHumidity = 60;
-            float avgSoilWetness = 300;
-
+            // Create a JSON response
             String json = "{";
-            json += "\"avg_brightness\": " + String(avgBrightness) + ",";
-            json += "\"avg_temperature\": " + String(avgTemperature) + ",";
-            json += "\"avg_humidity\": " + String(avgHumidity) + ",";
-            json += "\"avg_soil_wetness\": " + String(avgSoilWetness);
+            json += "\"avg_brightness\": " + String(brightness) + ",";
+            json += "\"avg_temperature\": " + String(temperature) + ",";
+            json += "\"avg_humidity\": " + String(humidity) + ",";
+            json += "\"avg_soil_wetness\": " + String(soilMoisture);
             json += "}";
 
+            // Send HTTP response headers
             client.println("HTTP/1.1 200 OK");
             client.println("Content-Type: application/json");
             client.println("Connection: close");
             client.println();
             client.println(json);
         }
-        else
+        else // Default web interface with sensor values
         {
+            // HTML response with styled sensor values
             String html = "<html><head>";
             html += "<title>Smart Irrigation</title>";
             html += "<style>";
@@ -58,15 +84,16 @@ void loop()
             html += "</head><body>";
             html += "<h2>Smart Irrigation</h2>";
             html += "<div class='container'>";
-            html += "<div class='box' id='brightness'>&#9728; Brightness: 512</div>";
-            html += "<div class='box' id='temperature'>&#127777; Temperature: 25°C</div>";
-            html += "<div class='box' id='humidity'>&#128167; Humidity: 60%</div>";
-            html += "<div class='box' id='soil'>&#127793; Soil Moisture: 300</div>";
+            html += "<div class='box' id='brightness'>&#9728; Brightness: " + String(brightness) + "</div>";
+            html += "<div class='box' id='temperature'>&#127777; Temperature: " + String(temperature) + "°C</div>";
+            html += "<div class='box' id='humidity'>&#128167; Humidity: " + String(humidity) + "%</div>";
+            html += "<div class='box' id='soil'>&#127793; Soil Moisture: " + String(soilMoisture == 0 ? "Wet" : "Dry") + "</div>";
             html += "</div>";
             html += "<p>Last Irrigation: 2 hours ago</p>";
             html += "<p>Last SMS Request: 30 minutes ago</p>";
             html += "</body></html>";
 
+            // Send HTTP response headers
             client.println("HTTP/1.1 200 OK");
             client.println("Content-Type: text/html");
             client.println("Connection: close");
@@ -75,18 +102,25 @@ void loop()
         }
     }
 
+    // Check if SIM800L has received an SMS message
     if (sim800l.available())
     {
-        String message = sim800l.readString();
-        if (message.indexOf("GET DATA") != -1)
+        String message = sim800l.readString(); // Read incoming message
+        if (message.indexOf("GET DATA") != -1) // If message contains "GET DATA"
         {
-            String response = "Brightness: 512\nTemperature: 25C\nHumidity: 60%\nSoil Moisture: 300";
-            sim800l.println("AT+CMGF=1");
+            // Sensor values to be sent via SMS
+            String response = "Brightness: " + String(brightness) + "\n";
+            response += "Temperature: " + String(temperature) + "C\n";
+            response += "Humidity: " + String(humidity) + "%\n";
+            response += "Soil Moisture: " + String(soilMoisture == 0 ? "Wet" : "Dry");
+
+            // Send SMS via SIM800L
+            sim800l.println("AT+CMGF=1"); // Set SMS mode
             delay(100);
-            sim800l.println("AT+CMGS=\"+1234567890\"");
+            sim800l.println("AT+CMGS=\"+1234567890\""); // Replace with recipient number
             delay(100);
-            sim800l.print(response);
-            sim800l.write(26);
+            sim800l.print(response); // Send sensor data
+            sim800l.write(26); // End SMS (CTRL+Z)
         }
     }
 }
